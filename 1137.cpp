9f9968dbe2c3955eb11c05c629e932d8169e8456
Otrebus <panzerschlacht@hotmail.com>
2015-07-18 21:03:32 +0200

    a solution that is not horrid and actually works

--- 1137.cpp
+++ 1137.cpp
@@ -1,99 +1,50 @@
 /* 1137. Bus Routes - http://acm.timus.ru/problem.aspx?num=1137
  *
  * Strategy:
- * Maintain a linked list, and have each node point to the element in the linked list that we can
- * add another loop to. Do a depth-first search through the graph and add loops wherever we can.
- * This solution and its code is a horrid abomination.
+ * Find the Euler cycle by, at a given node, queue up all nodes reachable in single
+ * search path from this node, and after this, go through each node in the resulting 
+ * queue and do the same thing recursively. Each entrance into the recursion yields a new
+ * node of the Euler cycle.
  *
  * Performance:
- * O(mn), which is optimal since the input is of this size. I can't test the performance or the
- * validity of this algorithm since Timus is down.
+ * O(mn), which is optimal since the input is of this size. The solution runs at 0.001s which
+ * is the fastest possible time, and uses 516 KB memory.
  */
 
 #include <stdio.h>
 #include <vector>
-#include <unordered_set>
-
-struct node;
+#include <queue>
 
 struct vertex
 {
-    std::vector<std::pair<vertex*, bool>> out;
+    std::vector<vertex*> out;
     int n;
-    node* node;
-
-    vertex() : node(0), n(0)
-    {
-    }
 };
 
-struct node
-{
-    node* next, *prev;
-    vertex* v;
-
-    node(vertex* v, node* prev, node* next) : next(next), v(v), prev(prev) {}
-};
-
-void makeLoop(vertex* u, std::pair<vertex*, bool>* p, std::vector<node>& nodes)
-{   // Add a loop from u in the direction of p
-    node* original = u->node;
-    if(original) // We save the original outgoing to append it last, and create a new node to loop from
-    {
-        nodes.push_back(node(u, original->prev, 0));
-        u->node = &nodes.back();
-        original->prev->next = &nodes.back();
-        nodes.back().prev = original->prev;
-    }
-    else // Create a new node in the linked list since we had no one to start out with
-    {
-        nodes.push_back(node(u, 0, 0));
-        u->node = &nodes.back();
-        nodes.back().v = u;
-    }
+void explore(vertex* u, std::vector<vertex*>& ans)
+{   // Go as far as we can in some direction and queue up all nodes. We are guaranteed to return.
+    ans.push_back(u);
+    std::queue<vertex*> q;
     vertex* v = u;
-    vertex* w;
-
-    do  // Loop through the graph and build a linked list
-    {
-        w = p->first;
-        p->second = true;
-        nodes.push_back(node(w, v->node, 0));
-        w->node = &nodes.back();
-        v->node->next = w->node;
-
-        for(auto it = w->out.begin(); it < w->out.end(); it++)
-        {
-            if(!it->second)
-            {
-                it->second = true;
-                p = &(*it);
-                break;
-            }
-        }
+    while(!v->out.empty())
+    {   // I wish pop_back returned a value
+        auto w = v->out.back();
+        v->out.pop_back();
         v = w;
-    } while(p->first != u);
-
-    if(original) // Attach our built linked list back to the origin of this loop
-    {
-        v->node->next = original;
-        original->prev = v->node;
+        q.push(v);
     }
-    else
-    {
-        v->node->next = u->node;
-        u->node->prev = v->node;
+    while(!q.empty())
+    {   // For all queued nodes, recurse
+        explore(q.front(), ans);
+        q.pop();
     }
 }
 
 int main()
 {
     int n;
-    std::vector<vertex> v(10001);   // Graph nodes (I call them vertices for some reason)
-    std::vector<vertex*> s(100001); // Stack for the depth-first search
-    std::vector<node> nodes;        // The nodes of the linked list
-    nodes.reserve(100001);          // We do push_back and have pointers to inside the vector
-                                    // so make sure we don't resize it and invalidate all pointers
+    std::vector<vertex> v(10001); // Node storage
+
     scanf("%d", &n);
     int m, last, tot = 0;
 
@@ -105,37 +56,15 @@ int main()
         {
             int u;
             scanf("%d", &u);
-            tot++;
-            if(!v[u].n)
-                v[u].n = u;
-            v[last].out.push_back(std::make_pair(&v[u], false));
+            v[u].n = u;
+            v[last].out.push_back(&v[u]);
             last = u;
         }
     }
-
-    int top = 0;
-    s[top++] = &v[last];
-    while(top != 0) // Do depth-first search and spam loops wherever we can
-    {
-        vertex* u = s[--top];
-        for(auto it = u->out.begin(); it < u->out.end(); it++)
-        {
-            if(!it->second)
-            {
-                s[top++] = it->first;
-                makeLoop(u, &(*it), nodes);
-                it->second = true;
-            }
-        }
-
-    }
-    printf("%d ", tot);
-    auto node = &nodes.front();
-    while(tot--)
-    {
-        printf("%d ", node->v->n);
-        node = node->next;
-    }
-    printf("%d ", nodes.front().v->n);
+    std::vector<vertex*> ans;
+    explore(&v[last], ans);
+    printf("%d ", ans.size()-1);
+    for(auto it = ans.begin(); it < ans.end(); it++)
+        printf("%d ", (*it)->n);
     return 0;
 }
