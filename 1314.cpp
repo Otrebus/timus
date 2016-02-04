c2d464e7f84d2ca9aeba20b9ef67d3d59eeeca3c
Otrebus <panzerschlacht@hotmail.com>
2016-02-04 22:44:04 +0100

    1314

---
+++ 1314.cpp
@@ -0,0 +1,44 @@
+/* 1314. Chase in Subway - http://acm.timus.ru/problem.aspx?num=1314
+ *
+ * Strategy:
+ * Minmax + dp.
+ *
+ * Performance:
+ * O(nk), runs the test suite in 0.015s using 344KB memory.
+ */
+
+#include <stdio.h>
+#include <algorithm>
+
+std::pair<int, int> F[10001], W[10001];
+
+std::pair<int, int> minmax(int i, int n, int m, int k, std::pair<int, int>* W)
+{
+    std::pair<int, int> fmax = { -100000, 0 };
+    for(int j = i-1; j >= i-k; j--)
+    {
+        if(j < 0)
+        {
+            if(m > fmax.first)
+                fmax = { m, 0 };
+            break;
+        }
+        std::pair<int, int> pz = { i-j + W[j].second, W[j].first };
+        if(pz.first > fmax.first || 
+           pz.first == fmax.first && pz.second < fmax.second)
+            fmax = pz;
+    }
+    return fmax;
+}
+
+int main()
+{
+    int n, m, k;
+    scanf("%d %d %d", &n, &m, &k);
+    for(int i = 1; i <= n; i++)
+    {
+        F[i] = minmax(i, n, m, k, W);
+        W[i] = minmax(i, n, m, k, F);
+    }
+    printf("%d %d", W[n]);
+}
