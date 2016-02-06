82db4da7a477923c715e21ba6cccb66a1589cac7
Otrebus <panzerschlacht@hotmail.com>
2016-02-06 01:45:15 +0100

    1314

--- 1314.cpp
+++ 1314.cpp
@@ -1,44 +1,73 @@
 /* 1314. Chase in Subway - http://acm.timus.ru/problem.aspx?num=1314
  *
  * Strategy:
- * Minmax + dp.
+ * Do a BFS from the first tracked location of the thief to all locations, recording the depth of
+ * each node from the starting position. Then, do another search from the last tracked location of
+ * the thief to all nodes where the earlier recorded depths increase by one at each jump. The nodes
+ * traversed by the last search constitute the answer.
  *
  * Performance:
- * O(nk), runs the test suite in 0.015s using 344KB memory.
+ * O(NK log NK); runs the test suite in 0.015 using 968KB memory, rated 33/730.
  */
 
+#include <vector>
 #include <stdio.h>
-#include <algorithm>
+#include <set>
+#include <queue>
 
-std::pair<int, int> F[10001], W[10001];
+const int maxn = 32767;
 
-std::pair<int, int> minmax(int i, int n, int m, int k, std::pair<int, int>* W)
+struct Node
 {
-    std::pair<int, int> fmax = { -100000, 0 };
-    for(int j = i-1; j >= i-k; j--)
-    {
-        if(j < 0)
-        {
-            if(m > fmax.first)
-                fmax = { m, 0 };
-            break;
-        }
-        std::pair<int, int> pz = { i-j + W[j].second, W[j].first };
-        if(pz.first > fmax.first || 
-           pz.first == fmax.first && pz.second < fmax.second)
-            fmax = pz;
-    }
-    return fmax;
-}
+    std::vector<Node*> v;
+    int depth;
+} nodes[maxn+1];
 
 int main()
 {
-    int n, m, k;
-    scanf("%d %d %d", &n, &m, &k);
-    for(int i = 1; i <= n; i++)
+    for(int i = 0; i < maxn+1; i++)
+        nodes[i].depth = maxn;
+    int N, K, M, first, last, id, lastid;
+    scanf("%d", &N);
+    while(N--)
     {
-        F[i] = minmax(i, n, m, k, W);
-        W[i] = minmax(i, n, m, k, F);
+        scanf("%d %d", &K, &lastid);
+        while(--K)
+        {
+            scanf("%d", &id);
+            int p1 = std::min(id, lastid);
+            int p2 = std::max(id, lastid);
+            nodes[p1].v.push_back(nodes+p2);
+            nodes[p2].v.push_back(nodes+p1);
+            lastid = id;
+        }
     }
-    printf("%d %d", W[n]);
+    scanf("%d %d", &M, &first);
+    last = first;
+    while(--M)
+        scanf("%d", &last);
+    std::queue<Node*> Q;
+    Q.push(nodes+first);
+    nodes[first].depth = 0;
+    while(!Q.empty()) // First search from the starting point, records depths
+    {
+        auto node = Q.front();
+        Q.pop();
+        for(auto n : node->v)
+            if(n->depth > node->depth+1)
+                n->depth = node->depth+1, Q.push(n);
+    }
+    std::set<Node*> ans;
+    Q.push(nodes+last);
+    while(!Q.empty()) // Last search, traverse nodes with unit increase of depth
+    {
+        auto node = Q.front();
+        Q.pop();
+        ans.insert(node);
+        for(auto n : node->v)
+            if(n->depth == node->depth+1)
+                Q.push(n);
+    }
+    for(auto a : ans)
+        printf("%d\n", a-nodes);
 }
