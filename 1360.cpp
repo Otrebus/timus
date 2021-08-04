f8031feec172e0a21455eda95ee392aac7969595
Peter Otrebus-Larsson <otrebus@gmail.com>
2021-08-04 08:39:55 +0200

    1360

---
+++ 1360.cpp
@@ -0,0 +1,21 @@
+/* 1360. Philosophical Dispute - http://acm.timus.ru/problem.aspx?num=1360
+ *
+ * Strategy:
+ * Set t to acos(y) to get the y coordinate right, increase t by 2pi until x meets the precision.
+ *
+ * Performance:
+ * O(1/t), runs the tests in 0.015s using 16KB memory.
+ */
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+int main() {
+    long double x, y, eps;
+    std::cin >> x >> y >> eps;
+
+    for(long double t = std::acos(y); ;t += 6.28318530718)
+        if(std::abs(std::sin(std::sqrt(t))-x) < eps)
+            return std::cout << std::fixed << std::setprecision(10) << t, 0;
+}
