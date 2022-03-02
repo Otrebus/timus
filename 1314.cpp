bc38ffe7928d25915b11e9a1666539a88684272b
Peter Otrebus-Larsson <otrebus@gmail.com>
2022-03-02 08:25:20 +0100

    1314 fix

--- 1314.cpp
+++ 1314.cpp
@@ -7,7 +7,7 @@
  * traversed by the last search constitute the answer.
  *
  * Performance:
- * O(NK log NK); runs the test suite in 0.015 using 968KB memory, rated 33/730.
+ * O(NK + Nlog N); runs the test suite in 0.015 using 1,444KB memory.
  */
 
 #include <vector>
@@ -17,56 +17,55 @@
 
 const int maxn = 32767;
 
-struct Node
-{
-    std::vector<Node*> v;
-    int depth;
+struct Node {
+    std::vector<Node*> out; // Edges
+    int d; // Depth
+    bool v; // Visited
 } nodes[maxn+1];
 
 int main()
 {
     for(int i = 0; i < maxn+1; i++)
-        nodes[i].depth = maxn;
+        nodes[i].d = maxn;
     int N, K, M, first, last, id, lastid;
+
     scanf("%d", &N);
-    while(N--)
-    {
-        scanf("%d %d", &K, &lastid);
-        while(--K)
-        {
-            scanf("%d", &id);
-            int p1 = std::min(id, lastid);
-            int p2 = std::max(id, lastid);
-            nodes[p1].v.push_back(nodes+p2);
-            nodes[p2].v.push_back(nodes+p1);
-            lastid = id;
+    while(N--) {
+        scanf("%d", &K);
+        std::vector<int> ids;
+        while(K--)
+            scanf("%d", &id), ids.push_back(id);
+        for(int i = 0; i < ids.size()-1; i++) {
+            nodes[ids[i]].out.push_back(nodes+ids[i+1]);
+            nodes[ids[i+1]].out.push_back(nodes+ids[i]);
         }
     }
+
     scanf("%d %d", &M, &first);
     last = first;
     while(--M)
         scanf("%d", &last);
+
     std::queue<Node*> Q;
     Q.push(nodes+first);
-    nodes[first].depth = 0;
-    while(!Q.empty()) // First search from the starting point, records depths
-    {
+    nodes[first].d = 0;
+    while(!Q.empty()) { // First search from the starting point, records depths
         auto node = Q.front();
         Q.pop();
-        for(auto n : node->v)
-            if(n->depth > node->depth+1)
-                n->depth = node->depth+1, Q.push(n);
+        for(auto n : node->out)
+            if(n->d > node->d+1)
+                n->d = node->d+1, Q.push(n);
     }
+
     std::set<Node*> ans;
     Q.push(nodes+last);
-    while(!Q.empty()) // Last search, traverse nodes with unit increase of depth
-    {
+    while(!Q.empty()) { // Last search, traverse nodes with unit increase of depth
         auto node = Q.front();
         Q.pop();
         ans.insert(node);
-        for(auto n : node->v)
-            if(n->depth == node->depth+1)
-                Q.push(n);
+        for(auto n : node->out)
+            if(n->d == node->d+1 && !n->v)
+                Q.push(n), n->v = true;
     }
     for(auto a : ans)
         printf("%d\n", a-nodes);
