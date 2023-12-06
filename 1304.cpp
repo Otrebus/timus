83e291b62335666f7900d141821fba389eb2c6be
Peter Otrebus-Larsson <otrebus@gmail.com>
2023-12-06 20:51:45 +0100

    1304 v2

--- 1304.cpp
+++ 1304.cpp
@@ -1,83 +1,111 @@
 /* 1304. Parallelepiped - http://acm.timus.ru/problem.aspx?num=1304
  *
  * Strategy:
- * Sort the points by their z coordinates and add each x and y coordinate to a set. For each pair
- * of (x, y)-coordinates, iterate (in z-order) over the points inside the axis-aligned box implied
- * by the pair and update the maximum overall volume with the volume of the axis-aligned
- * parallelepiped formed by the box and the z-coordinates of the two most recent points.
- * 
+ * For each pair of z-coordinates given in the points and boundaries, project all points between
+ * those z-coordinates onto the xy plane and find the largest empty rectangle in quadratic time;
+ * the answer is the maximum volume formed by the parallelepiped formed by any such z-interval and
+ * its maximum rectangle.
+ *
  * Performance:
- * O(n^5), runs the tests in 0.453s using 188KB memory.
+ * O(n^4), runs the tests in 0.015s using 200KB memory.
  */
 
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
+#include <iterator>
+#include <unordered_set>
 
 using ld = long double;
 using ll = long long;
-ll u, v, w;
+ll u, v, w, ans = 0;
 int n;
-ll ans = 0;
-
-struct point { 
-    ll x, y, z; 
-    point(ll x, ll y, ll z) : x(x), y(y), z(z) {}
-};
-
+struct point { ll x, y, z; };
 std::vector<point> P;
-std::vector<ll> X, Y;
 
-void calcZ(ll x1, ll x2, ll y1, ll y2) {
-    // Iterates in z-order inside the given box and finds the maximum slice between any consecutive
-    // z-coordinates of the points
-    ll z1 = 0, z2 = 0;
-    for(auto& p : P) {
-        if(p.z == 0 || p.z == w || p.y > y1 && p.y < y2 && p.x > x1 && p.x < x2) {
-            z1 = z2;
-            z2 = p.z;
-            ans = std::max(ans, (z2-z1)*(y2-y1)*(x2-x1));
+ll calc(ll x1, ll x2, ll y1, ll y2, ll z1, ll z2) {
+    // Calculates the area of the largest empty rectangle in the xy plane inside the given
+    // bounding box with all points between z1 and z2 projected into it
+    std::vector<point> v;
+    auto fn = [&z1, &z2](auto& p) { return p.z > z1 && p.z < z2; };
+    std::copy_if(P.begin(), P.end(), std::back_inserter(v), fn);
+
+    auto n = v.size();
+    if(!n)
+        return (y2-y1)*(x2-x1);
+
+    // Find rectangles between the top and bottom sides by finding the smallest span between
+    // any x coordinates and multiplying by the height
+    std::sort(v.begin(), v.end(), [] (const point& a, const point& b) { return a.x < b.x; });
+    ll w = std::max(v[0].x-x1, x2-v.back().x);
+    for(int i = 1; i < n; i++)
+        w = std::max(w, v[i].x-v[i-1].x);
+    ll maxb = (y2-y1)*w;
+
+    // Find rectangles between points, and between points and the bottom
+    std::sort(v.begin(), v.end(), [] (const point& a, const point& b) { return a.y > b.y; });
+    for(int i = 0; i < n; i++) {
+        ll l = x1, r = x2;
+        for(int j = i+1; j < n; j++) {
+            // We start with a rectangle that spans the x-width of the xy plane starting at v[i]
+            // and then grows downwards but gets chopped off by each v[j] below; this finds every
+            // rectangle that has its top edge at v[i] and left/right edges bounded by other points
+            if(v[j].x > l && v[j].x < r) {
+                maxb = std::max(maxb, (r-l)*(v[i].y-v[j].y));
+                if(v[j].x >= v[i].x)
+                    r = std::min(r, v[j].x);
+                else if(v[j].x <= v[i].x)
+                    l = std::max(l, v[j].x);
+            }
         }
+        // Finally multiply by the distance to the bottom
+        maxb = std::max(maxb, (r-l)*(v[i].y-y1));
     }
+
+    // Find rectangles between the top and spanning as far as possible dowwnards between each pair
+    // of other points
+    for(int i = v.size()-1; i >= 0; i--) {
+        ll l = x1, r = x2;
+        for(int j = i-1; j >= 0; j--) {
+            if(v[j].x >= v[i].x)
+                r = std::min(r, v[j].x);
+            else if(v[j].x <= v[i].x)
+                l = std::max(l, v[j].x);
+        }
+        maxb = std::max(maxb, (r-l)*(y2-v[i].y));
+    }
+
+    return maxb;
 }
 
-ll round(ll ans) {
-    // Rounds the fixed-point number in the natural way
+ll div(ll ans) { // Rounds the integers into fixed-point output
     return (ans+5000)/10000;
 }
 
 int main()
 {
     ld x, y, z;
-
-    // Read stuff and turn them to fixed-point integers
     scanf("%lf %lf %lf %d", &x, &y, &z, &n);
+
+    // We turn all the input into integers by multiplying by 100
     u = x*100 + 0.1, v = y*100 + 0.1, w = z*100 + 0.1;
+    std::unordered_set<ll> S; // All the z-coordinates
     for(int i = 0; i < n; i++) {
         scanf("%lf %lf %lf", &x, &y, &z);
-        x = x*100+0.1, y = y*100+0.1, z = z*100+0.1;
-        if(x >= 0 && x <= u && y >= 0 && y <= v && z >= 0 && z <= w)
-            P.emplace_back(x, y, z);
+        ll a = x*100+0.1, b = y*100+0.1, c = z*100+0.1;
+        if(a >= 0 && a <= u && b >= 0 && b <= v && c >= 0 && c <= w) {
+            P.push_back({ a, b, c });
+            S.insert(c);
+        }
     }
 
-    P.insert(P.end(), { { 0, 0, 0 }, { u, v, w } }); // Add boundary points
+    S.insert({ 0, w });
+    for(auto z1 : S)
+        for(auto z2 : S)
+            if(z2 > z1)
+                ans = std::max(ans, (z2-z1)*calc(0, u, 0, v, z1, z2));
 
-    // Make sorted lists of coordinates and sort points by Z
-    for(auto& p : P)
-        X.push_back(p.x), Y.push_back(p.y);
-    std::sort(X.begin(), X.end());
-    std::sort(Y.begin(), Y.end());
-    std::sort(P.begin(), P.end(), [] (point& a, point& b) { return a.z < b.z; });
-    X.erase(std::unique(X.begin(), X.end()), X.end());
-    Y.erase(std::unique(Y.begin(), Y.end()), Y.end());
-
-    // For each pair of (x, y), find the biggest z-slice
-    for(int i = 0; i < X.size(); i++)
-        for(int j = i+1; j < X.size(); j++)
-            for(int k = 0; k < Y.size(); k++)
-                for(int l = k+1; l < Y.size(); l++)
-                    calcZ(X[i], X[j], Y[k], Y[l]);
-
-    ans = round(ans);
+    ans = div(ans);
     printf("%lld.%02lld\n", ans/100, ans%100);
+    return ans/100;
 }
